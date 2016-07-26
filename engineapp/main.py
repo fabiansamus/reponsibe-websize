3ad871f5a26b72411668a9f105c430f9f907161f
Abiezer Reyes <master21delakal@live.com>
2016-07-26 15:55:16 -0400

    unidad 3

--- engineapp/main.py
+++ engineapp/main.py
@@ -17,17 +17,31 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
-class MainPage(Handler):
-	def get(self):
-		items=self.request.get_all("food")
-		self.render("shopping_list.html",items = items)
-
-class FizzBuzzHandler(Handler):
-	def get(self):
-		n= self.request.get("n",0)
-		n= n and int(n)
-		self.render('fizzbuzz.html',n=n)
-	
+class Art(db.Model):
+	title=db.StringProperty(required=True)
+	art=db.TextProperty(required=True)
+	created=db.DateTimeProperty(auto_now_add=True)
 		
 
-app = webapp2.WSGIApplication([('/', MainPage),('/fizzbuzz',FizzBuzzHandler)],debug=True)
+class MainPage(Handler):
+	def render_front(self, title="", art="", error=""):
+		art= db.GqlQuery("")
+		self.render("front.html",title=title,art=art,error=error)
+	def get(self):
+		self.render_front()
+	def pos(self):
+		title= self.request.get("title")
+		art= self.request.get("art")
+
+		if title and art:
+			a=Art(title=get("title"))
+			a.put()
+
+			self.redirect("/")
+		else:
+			error = "we need both a title and some artwork!"
+			self.render_front("front.html",title,art,error)
+		
+		
+
+app = webapp2.WSGIApplication([('/', MainPage)],debug=True)
